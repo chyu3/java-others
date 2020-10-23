a2d1692f1c827780149424e9358de09926cfa0ab
Tunax-C <ariestina.tc@gmail.com>
2020-10-23 15:10:29 +0800

    summary

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -27,22 +27,20 @@ public class FizzBuzz
 	public static void main (String[] args) 
 	{
 		for (int i = 1; i <= 15; i++)
-
+		if (i%3 == 0 && i%5 == 0)
+		{
+			System.out.println("FizzBuzz");
+		}
+		
+		else if (i%3 == 0)
+		{
+			System.out.println("Fizz");
+		}
 			
-			if (i%3 == 0 && i%5 == 0)
-			{
-				System.out.println("FizzBuzz");
-			}
-			
-			else if (i%3 == 0)
-			{
-				System.out.println("Fizz");
-			}
-			
-			else
-			{
-				System.out.println(i);
-			}
+		else
+		{
+			System.out.println(i);
+		}
 		
 	}
 }
